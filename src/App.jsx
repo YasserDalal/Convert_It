2cb09fb9c4f073d6a773e9d8257e32d3cbfd123b
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-06 20:45:03 +0300

    import the 2 component layouts in a root component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 
+import TopLayout from "./shared/layouts/TopLayout"
+import CenterLayout from "./shared/layouts/CenterLayout"
 export default function App() {
   return (
-    <div className="bg-gray-900 h-screen grid place-items-center">
-      
+    <div className="bg-gray-900 h-screen text-white">
+      <TopLayout />
+      <CenterLayout />
     </div>
   )
 }
