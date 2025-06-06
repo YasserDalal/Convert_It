d798e2248d7f990733a26a0d2dbb3b0a0431c0ba
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-06 20:43:54 +0300

    create 2 components for layouts

---
+++ src/shared/layouts/CenterLayout.jsx
@@ -0,0 +1,9 @@
+
+import CenterContent from "../../components/CenterContent"
+export default function CenterLayout() {
+  return (
+    <div className="relative top-20 h-full flex justify-center items-center">
+      <CenterContent />
+    </div>
+  )
+} 
---
+++ src/shared/layouts/TopLayout.jsx
@@ -0,0 +1,9 @@
+
+import TopContent from "../../components/TopContent"
+export default function TopLayout() {
+  return (
+    <div className="fixed top-0 left-16 right-16 h-16 rounded-bl-lg rounded-br-lg bg-gray-800 shadow-[1px_1px_10px_1px] shadow-[#040404f1] ">
+      <TopContent />
+    </div>
+  )
+}
