1d563789a1d980cae62b615e03aa2467439d61f4
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-07 18:23:45 +0300

    adjust the width of the TopLayout component when the screen shrinks

--- src/shared/layouts/TopLayout.jsx
+++ src/shared/layouts/TopLayout.jsx
@@ -2,7 +2,7 @@
 import TopContent from "../../components/TopContent"
 export default function TopLayout() {
   return (
-    <div className="fixed top-0 left-16 right-16 h-16 rounded-bl-lg rounded-br-lg bg-gray-800 shadow-[1px_1px_10px_1px] shadow-[#040404f1] ">
+    <div className="fixed top-0 left-16 right-16 h-16 rounded-bl-lg rounded-br-lg bg-gray-800 shadow-[1px_1px_10px_1px] shadow-[#040404f1] max-[440px]:left-10 max-[440px]:right-10">
       <TopContent />
     </div>
   )
