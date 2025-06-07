5f4989ccf3dc36bdcb1926803803b584aa19769c
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-07 18:19:43 +0300

    make the TopContent component responsive

--- src/components/TopContent.jsx
+++ src/components/TopContent.jsx
@@ -1,11 +1,12 @@
 
 export default function TopContent() {
   return (
-    <div className="flex justify-between px-10 h-full">
+    <div className="flex justify-between px-10 h-full max-[440px]:px-4">
       <div className="font-tab italic text-4xl flex items-center">
         <div>Convert It</div>
       </div>
-      <div className="flex gap-4 items-center text-lg">
+      <div className="hidden max-[720px]:block">Hi</div>
+      <div className="flex gap-4 items-center text-lg max-[720px]:hidden">
         <div className="cursor-pointer">Temperature</div>
         <div className="cursor-pointer">BMI</div>
         <div className="cursor-pointer">TIME</div>
