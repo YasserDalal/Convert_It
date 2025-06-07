1bcd7192a5c1bba50aa6bff0ffc0c076c5828161
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-07 18:34:22 +0300

    adjust the responsiveness of Text in TitleContent component

--- src/components/TitleContent.jsx
+++ src/components/TitleContent.jsx
@@ -3,8 +3,8 @@ export default function TitleContent({ classname }) {
   return (
     <div className={ classname }>
       <div className="flex flex-col">
-        <div className="text-4xl pb-3 text-[clamp(1.2rem,6vw,2.5rem)]">Temperature Converter</div>
-        <div className="text-xl text-[clamp(0.5rem,2.5vw,1rem)]">Enter the temperature, select units and submit.</div>
+        <div className="pb-3 text-[clamp(1.5rem,8vw,2.8rem)]">Temperature Converter</div>
+        <div className="text-xl text-[clamp(0.72rem,3vw,1rem)]">Enter the temperature, select units and submit.</div>
       </div>
     </div>
   )
