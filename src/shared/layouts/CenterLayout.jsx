53b2e49e01e3874bd482a4440b009f865ee02623
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-07 18:34:57 +0300

    adjust the media queries, to avoid text wraping

--- src/shared/layouts/CenterLayout.jsx
+++ src/shared/layouts/CenterLayout.jsx
@@ -3,9 +3,9 @@ import TitleContent from "../../components/TitleContent"
 import InputContent from "../../components/InputContent"
 export default function CenterLayout() {
   return (
-    <div className="h-full w-full flex items-center max-[1107px]:content-start">
-      <div className="flex justify-center items-center max-[1107px]:flex-col min-w-[320px] w-full">
-        <TitleContent classname={"pr-56 pb-28 max-[1107px]:pb-40 max-[1107px]:pr-0"}/>
+    <div className="h-full w-full flex items-center max-[1174px]:content-start">
+      <div className="flex justify-center items-center max-[1174px]:flex-col min-w-[320px] w-full">
+        <TitleContent classname={"pr-56 pb-28 max-[1174px]:pb-40 max-[1174px]:pr-0"}/>
         <InputContent />
       </div>
     </div>
