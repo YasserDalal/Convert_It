cbc19c4f05f34e5b62616ba0e967409c256d2372
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-07 18:13:24 +0300

    make the root component responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TopLayout from "./shared/layouts/TopLayout"
 import CenterLayout from "./shared/layouts/CenterLayout"
 export default function App() {
   return (
-    <div className="bg-gray-900 h-screen text-white">
+    <div className="bg-gray-900 h-screen text-white min-w-[320px] max-w-full w-full">
       <TopLayout />
       <CenterLayout />
     </div>
