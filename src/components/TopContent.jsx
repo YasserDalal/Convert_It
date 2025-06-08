98234854f00481094d82847a6f1c2aab8e037cbd
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-08 18:33:56 +0300

    get the props, and pass them to the child, make the navigation buttons dynamic

--- src/components/TopContent.jsx
+++ src/components/TopContent.jsx
@@ -1,8 +1,6 @@
 
-import { useState } from "react"
-
-export default function TopContent() {
-  const [clicked, setClicked] = useState('Temperature');
+import ClickedNav from "../events/NavButtons/ClickedNav";
+export default function TopContent({ navigatorNames, clicked, setClicked }) {
   return (
     <div className="flex justify-between px-10 h-full max-[810px]:px-4">
       <div className="font-tab italic text-4xl flex items-center">
@@ -10,11 +8,9 @@ export default function TopContent() {
       </div>
       <div className="hidden max-[720px]:block">Hi</div>
       <div className="flex gap-3 items-center text-lg max-[720px]:hidden">
-        <div className={`hover:text-white transition-colors duration-150 px-2 py-4 ${clicked === 'Temperature' ? 'text-white select-none':'text-[#b0b0b0] cursor-pointer'}`} typeof="button">Temperature</div>
-        <div className={`hover:text-white transition-colors duration-150 px-2 py-4 ${clicked === 'BMI' ? 'text-white select-none':'text-[#b0b0b0] cursor-pointer'}`} typeof="button">BMI</div>
-        <div className={`hover:text-white transition-colors duration-150 px-2 py-4 ${clicked === 'Time' ? 'text-white select-none':'text-[#b0b0b0] cursor-pointer'}`} typeof="button">Time</div>
-        <div className={`hover:text-white transition-colors duration-150 px-2 py-4 ${clicked === 'Math' ? 'text-white select-none':'text-[#b0b0b0] cursor-pointer'}`} typeof="button">Math</div>
-        <div className={`hover:text-white transition-colors duration-150 px-2 py-4 ${clicked === 'Money' ? 'text-white select-none':'text-[#b0b0b0] cursor-pointer'}`} typeof="button">Money</div>
+        {navigatorNames.map((item, index) => (
+          <div className={`hover:text-white transition-colors duration-150 px-2 py-4 ${clicked === `${item.name}` ? 'text-white select-none':'text-[#b0b0b0] cursor-pointer'}`} typeof="button" onClick={() => ClickedNav(setClicked, item)} key={index}>{item.name}
+          </div>))}
       </div>
     </div>
   )
