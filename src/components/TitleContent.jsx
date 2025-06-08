a5251c7032285aabc467b3073828aa36153de392
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-08 18:37:09 +0300

    get the props as well then pass them, make the title of CenterLayout dynamic

--- src/components/TitleContent.jsx
+++ src/components/TitleContent.jsx
@@ -1,10 +1,22 @@
 
-export default function TitleContent({ classname }) {
+import React from "react"
+
+export default function TitleContent({ classname, contents, clicked }) {
+  contents.filter(obj => obj[clicked] && console.log(obj[clicked][0].title))
   return (
     <div className={ classname }>
       <div className="flex flex-col max-[560px]:px-5">
-        <div className="pb-3 text-[clamp(1.5rem,8vw,2.8rem)] max-[560px]:text-center">Temperature Converter</div>
-        <div className="text-xl text-[clamp(0.73rem,3.5vw,1.1rem)] max-[560px]:text-center">Enter the temperature, select units and submit.</div>
+        { contents.filter(obj => obj[clicked]).map((obj, index) =>
+          (
+          <React.Fragment key={index}>
+            <div className="pb-3 text-[clamp(1.5rem,8vw,2.8rem)] max-[560px]:text-center">
+              {obj[clicked][0].title}
+            </div>
+            <div className="text-xl text-[clamp(0.73rem,3.5vw,1.1rem)] max-[560px]:text-center">
+              Enter the temperature, select units and submit.
+            </div>
+          </React.Fragment>
+          ))}
       </div>
     </div>
   )
--- src/shared/layouts/CenterLayout.jsx
+++ src/shared/layouts/CenterLayout.jsx
@@ -1,12 +1,12 @@
 
 import TitleContent from "../../components/TitleContent"
 import InputContent from "../../components/InputContent"
-export default function CenterLayout() {
+export default function CenterLayout({ contents, clicked }) {
   return (
     <div className="h-full w-full flex items-center max-[1174px]:content-start">
       <div className="flex justify-center items-center max-[1174px]:flex-col min-w-[320px] w-full">
-        <TitleContent classname={"pr-56 pb-28 max-[1174px]:pb-40 max-[1174px]:pr-0"}/>
-        <InputContent />
+        <TitleContent classname={"pr-56 pb-28 max-[1174px]:pb-40 max-[1174px]:pr-0"} contents={contents} clicked={clicked}/>
+        <InputContent contents={contents} clicked={clicked}/>
       </div>
     </div>
   )
