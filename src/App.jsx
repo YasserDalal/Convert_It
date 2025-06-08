b0799158f9edca105dd16710b9aca98d7a50bfeb
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-08 18:30:38 +0300

    import the Data, transfer the states to the root, pass the props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 
 import TopLayout from "./shared/layouts/TopLayout"
 import CenterLayout from "./shared/layouts/CenterLayout"
+import { useState } from "react"
+import { Data } from "./data/Data"
 export default function App() {
+  const [navigatorNames, setNavigatorNames] = useState(Data[0].navigatorNames);
+  const [contents, setContents] = useState(Data[1].contents);
+  const [clicked, setClicked] = useState('Temperature');
+  console.log(contents)
+  console.log(clicked)
   return (
     <div className="bg-gray-900 h-screen text-white min-w-[320px] max-w-full w-full">
-      <TopLayout />
-      <CenterLayout />
+      <TopLayout navigatorNames={navigatorNames} clicked={clicked} setClicked={setClicked}/>
+      <CenterLayout contents={contents} clicked={clicked}/>
     </div>
   )
 }
