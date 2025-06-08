f82ad37a2a5714f656cd653514d4a1be484d4d25
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-08 21:40:28 +0300

    from Data.js to Data.jsx, in 'subtitle' property use jsx callbacks, instead of using strings

---
+++ src/data/Data.jsx
@@ -0,0 +1,63 @@
+
+export const Data = [
+  {
+    navigatorNames: [
+      { name: "Temperature" },
+      { name: "BMI" },
+      { name: "Time" },
+      { name: "Math" },
+      { name: "Money" }
+    ]
+  },
+  { 
+    contents: [
+      { Temperature: [
+          { title: 'Temperature Converter', subtitle: () => (<>Enter the <b>Temperature</b> select units and submit.</>)},
+          { units: [
+            { name: "Celsius", value: "°C" },
+            { name: "Fahrenheit", value: "°F" },
+            { name: "Kelvin", value: "K" }
+          ]}]
+      },
+      { BMI: [
+          { title: 'BMI Converter', subtitle: () => (<>Enter the <b>Weight</b> and <b>Height</b>, select units and submit.</>)},
+          { units: [
+            { name: "Metric", value: "kg/m2" },
+            { name: "Imperial", value: "lb/ft2" }
+          ]}
+        ]
+      },
+      { Time: [
+          { title: 'Time Converter', subtitle: () => (<>Enter the <b>Time</b>, select units and submit.</>)},
+          { units: [
+            { name: "Seconds", value: "Seconds" },
+            { name: "Minutes", value: "Minutes" },
+            { name: "Hours", value: "Hours" },
+            { name: "Days", value: "Days" },
+            { name: "Weeks", value: "Weeks" },
+            { name: "Months", value: "Months" },
+            { name: "Years", value: "Years" }
+          ]}
+        ]
+      },
+      { Math: [
+          { title: 'Math Converter', subtitle: () => (<>Enter the <b>Number</b>, select units and submit.</>)},
+          { units: [
+            { name: "Radians", value: "Radians" },
+            { name: "Degrees", value: "Degrees" },
+            { name: "Grades", value: "Grades" }
+          ]}
+        ]
+      },
+      { Money: [
+          { title: 'Money Converter', subtitle: () => (<>Enter the <b>Amount</b>, select currency and submit.</>)},
+          { units: [
+            { name: "Dollars", value: "$" },
+            { name: "Euros", value: "€" },
+            { name: "Pounds", value: "£" }
+          ]}
+        ]
+      }
+    ]
+  }
+]
