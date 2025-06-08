4a0160b5a653c34cd95f237bc30bd6197689ce4f
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-08 15:15:33 +0300

    fix a bug, make the style of the buttons dynamic

--- src/components/TopContent.jsx
+++ src/components/TopContent.jsx
@@ -1,17 +1,20 @@
 
+import { useState } from "react"
+
 export default function TopContent() {
+  const [clicked, setClicked] = useState('Temperature');
   return (
-    <div className="flex justify-between px-10 h-full max-[440px]:px-4">
+    <div className="flex justify-between px-10 h-full max-[810px]:px-4">
       <div className="font-tab italic text-4xl flex items-center">
         <div>Convert It</div>
       </div>
       <div className="hidden max-[720px]:block">Hi</div>
-      <div className="flex gap-4 items-center text-lg max-[720px]:hidden">
-        <div className="cursor-pointer">Temperature</div>
-        <div className="cursor-pointer">BMI</div>
-        <div className="cursor-pointer">TIME</div>
-        <div className="cursor-pointer">MATH</div>
-        <div className="cursor-pointer">MONEY</div>
+      <div className="flex gap-3 items-center text-lg max-[720px]:hidden">
+        <div className={`hover:text-white transition-colors duration-150 px-2 py-4 ${clicked === 'Temperature' ? 'text-white select-none':'text-[#b0b0b0] cursor-pointer'}`} typeof="button">Temperature</div>
+        <div className={`hover:text-white transition-colors duration-150 px-2 py-4 ${clicked === 'BMI' ? 'text-white select-none':'text-[#b0b0b0] cursor-pointer'}`} typeof="button">BMI</div>
+        <div className={`hover:text-white transition-colors duration-150 px-2 py-4 ${clicked === 'Time' ? 'text-white select-none':'text-[#b0b0b0] cursor-pointer'}`} typeof="button">Time</div>
+        <div className={`hover:text-white transition-colors duration-150 px-2 py-4 ${clicked === 'Math' ? 'text-white select-none':'text-[#b0b0b0] cursor-pointer'}`} typeof="button">Math</div>
+        <div className={`hover:text-white transition-colors duration-150 px-2 py-4 ${clicked === 'Money' ? 'text-white select-none':'text-[#b0b0b0] cursor-pointer'}`} typeof="button">Money</div>
       </div>
     </div>
   )
