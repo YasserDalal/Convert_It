15355a2e973bf41f462c3b765ac2c97863d26541
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-08 14:16:20 +0300

    when texts starts to shrink, use text center

--- src/components/TitleContent.jsx
+++ src/components/TitleContent.jsx
@@ -2,9 +2,9 @@
 export default function TitleContent({ classname }) {
   return (
     <div className={ classname }>
-      <div className="flex flex-col">
-        <div className="pb-3 text-[clamp(1.5rem,8vw,2.8rem)]">Temperature Converter</div>
-        <div className="text-xl text-[clamp(0.72rem,3vw,1rem)]">Enter the temperature, select units and submit.</div>
+      <div className="flex flex-col max-[560px]:px-5">
+        <div className="pb-3 text-[clamp(1.5rem,8vw,2.8rem)] max-[560px]:text-center">Temperature Converter</div>
+        <div className="text-xl text-[clamp(0.73rem,3.5vw,1.1rem)] max-[560px]:text-center">Enter the temperature, select units and submit.</div>
       </div>
     </div>
   )
