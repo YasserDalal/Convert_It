56673cf731d4e3ac2eb07010fa86e5ab2829eca7
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-08 22:57:40 +0300

    use items-end

--- src/components/InputContent.jsx
+++ src/components/InputContent.jsx
@@ -7,7 +7,7 @@ export default function InputContent(){
   const [selectedFrom, setSelectedFrom] = useState("");
   const [selectedTo, setSelectedTo] = useState("");
   return (
-    <div className="flex gap-2 max-[520px]:flex-col max-[520px]:items-center max-[520px]:w-[90%]">
+    <div className="flex gap-2 items-end max-[520px]:flex-col max-[520px]:w-[90%]">
       <div className="w-full flex justify-center pb-4">
         <input type="text" inputMode="decimal" pattern="^\d{1,3}(\.\d{0,2})?$" maxLength={6} value={inputValue}  
              className="w-28 max-[520px]:w-[30%] h-12 rounded-lg bg-gray-700 px-2 text-white text-center text-2xl outline-none focus:shadow-xl focus:shadow-[#000000] cursor-pointer transition-all duration-150 hover:brightness-90" onInput={(e) => NumberInput(e, setInputValue)} placeholder="0.00"/>
