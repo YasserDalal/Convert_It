f8c80a682fc04f86a81a4cea6e25a41a28db6332
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-09 16:28:34 +0300

    adjust the spacing, import 'useEffect' hook,  use the 'localStorage' functions and  remove unnecessary setters

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { useState, useEffect } from "react"
+import { Data } from "./data/Data"
 
 import TopLayout from "./shared/layouts/TopLayout"
 import CenterLayout from "./shared/layouts/CenterLayout"
-import { useState } from "react"
-import { Data } from "./data/Data"
+
+import saveStorage from "./localStorage/saveStorage"
+import getStorage from "./localStorage/getStorage"
+
 export default function App() {
-  const [navigatorNames, setNavigatorNames] = useState(Data[0].navigatorNames);
-  const [contents, setContents] = useState(Data[1].contents);
-  const [clicked, setClicked] = useState('Temperature');
-  console.log(contents)
-  console.log(clicked)
+  // get the data from local storage
+  const [navigatorNames] = useState(getStorage('navigatorNames') || Data[0].navigatorNames);
+  const [contents] = useState(getStorage('contents') || Data[1].contents);
+  const [clicked, setClicked] = useState(getStorage('clicked') || 'Temperature');
+
+  // save the data when we refresh
+  useEffect(() => { saveStorage('navigatorNames', navigatorNames) }, [navigatorNames]);
+  useEffect(() => { saveStorage('contents', contents) }, [contents]);
+  useEffect(() => { saveStorage('clicked', clicked) }, [clicked]);
+
   return (
     <div className="bg-gray-900 h-screen text-white min-w-[320px] max-w-full w-full">
       <TopLayout navigatorNames={navigatorNames} clicked={clicked} setClicked={setClicked}/>
