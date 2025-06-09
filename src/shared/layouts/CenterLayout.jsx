04abba954f74f383f841af449283e79785947609
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-09 16:35:13 +0300

    fix the layout of 'CenterLayout' component

--- src/shared/layouts/CenterLayout.jsx
+++ src/shared/layouts/CenterLayout.jsx
@@ -4,8 +4,8 @@ import InputContent from "../../components/InputContent"
 export default function CenterLayout({ contents, clicked }) {
   return (
     <div className="h-full w-full flex items-center max-[1174px]:content-start">
-      <div className="flex justify-center items-center max-[1174px]:flex-col min-w-[320px] w-full">
-        <TitleContent classname={"pr-56 pb-28 max-[1174px]:pb-40 max-[1174px]:pr-0"} contents={contents} clicked={clicked}/>
+      <div className="flex justify-between items-center max-[1174px]:flex-col min-w-[320px] w-full px-28 max-[1174px]:px-0">
+        <TitleContent classname={"pb-28 max-[1174px]:pb-40 max-[1174px]:pr-0"} contents={contents} clicked={clicked}/>
         <InputContent contents={contents} clicked={clicked}/>
       </div>
     </div>
