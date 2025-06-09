b378efedff1047a3b077d179a18fbecfb4c28019
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-09 16:36:01 +0300

    fix a typo comment

--- src/utils/formatter/BoldInsideStrings.jsx
+++ src/utils/formatter/BoldInsideStrings.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function boldInsideStrings(text) {
   const string = text.split(/(<b>.*?<\/b>)/g);
   return string.map((words, i) => {
-    // check if there are bold tags *<br></br>* inside the string
+    // check if there are bold tags *<b></b>* inside the string
     if (words.startsWith('<b>') && words.endsWith('</b>')) {
       return <b key={i}>{words.slice(3, -4)}</b>;
     }
