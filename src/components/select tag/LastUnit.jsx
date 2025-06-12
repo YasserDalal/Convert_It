c33846f50f7439076bd810a485d277aac8ebe591
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-12 14:29:52 +0300

    remove outline when clicked

--- src/components/select tag/LastUnit.jsx
+++ src/components/select tag/LastUnit.jsx
@@ -1,7 +1,7 @@
 
 export default function LastUnit({ contents, clicked, selectedTo, selectedFrom, setSelectedTo }) {
   return (
-    <select name="cars" className="w-28 max-[520px]:w-full h-12 rounded-lg px-2 bg-gray-700 text-white focus:shadow-xl cursor-pointer transition-all duration-150 hover:brightness-90" onChange={(e) => setSelectedTo(e.target.value)} value={selectedTo} 
+    <select name="cars" className="w-28 max-[520px]:w-full h-12 rounded-lg px-2 bg-gray-700 text-white focus:shadow-xl cursor-pointer transition-all duration-150 hover:brightness-90 outline-none" onChange={(e) => setSelectedTo(e.target.value)} value={selectedTo} 
       style={{ backgroundImage: 'none', WebkitAppearance: 'none', MozAppearance: 'none',  appearance: 'none' }}>
       {contents.filter(obj => obj[clicked])
         .map((obj) => obj[clicked][1].units
