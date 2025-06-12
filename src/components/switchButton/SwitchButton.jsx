f29e1800e9a507c73653549c074a097a0773c5d9
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-12 13:48:30 +0300

    use icon from fontawesome in 'SwitchButton' component

--- src/components/switchButton/SwitchButton.jsx
+++ src/components/switchButton/SwitchButton.jsx
@@ -1,6 +1,11 @@
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
+
 export default function SwitchButton({ selectedFrom, selectedTo, setSelectedFrom, setSelectedTo }) {
   return (
-    <button className="w-28 max-[520px]:w-full h-12 rounded-lg bg-gray-800 cursor-pointer hover:bg-slate-700 transition-all duration-150" onClick={() => {setSelectedFrom(selectedTo); setSelectedTo(selectedFrom)}}>Switch</button>
+    <button className="w-16 max-[520px]:w-full h-12 rounded-full bg-gray-800 cursor-pointer hover:bg-slate-700 transition-all duration-150" onClick={() => {setSelectedFrom(selectedTo); setSelectedTo(selectedFrom)}}>
+      <FontAwesomeIcon icon={faArrowsRotate} />
+    </button>
   )
 } 
