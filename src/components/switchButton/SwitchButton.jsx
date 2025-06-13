27773baece17c7adea28ddd4f005b4307b2efd07
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-13 19:55:40 +0300

    switch units

--- src/components/switchButton/SwitchButton.jsx
+++ src/components/switchButton/SwitchButton.jsx
@@ -2,9 +2,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
 
-export default function SwitchButton({ selectedFrom, selectedTo, setSelectedFrom, setSelectedTo }) {
+export default function SwitchButton({ selectedFrom, selectedTo, setSelectedFrom, setSelectedTo, unitsFrom, setUnitsFrom, unitsTo, setUnitsTo }) {
   return (
-    <button className="w-16 max-[520px]:w-full h-12 rounded-full bg-gray-800 cursor-pointer hover:bg-slate-700 transition-all duration-150" onClick={() => {setSelectedFrom(selectedTo); setSelectedTo(selectedFrom)}}>
+    <button className="w-44 h-12 rounded-full bg-gray-800 cursor-pointer hover:bg-slate-700 transition-all duration-150" onClick={() => {setSelectedFrom(selectedTo); setSelectedTo(selectedFrom); setUnitsFrom(unitsTo); setUnitsTo(unitsFrom)}}>
       <FontAwesomeIcon icon={faArrowsRotate} />
     </button>
   )
