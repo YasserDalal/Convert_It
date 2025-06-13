24847601b9d9849aada1fe70b9b85e6a440ca62f
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-13 19:41:47 +0300

    make functionality to the Convert function, component imports it and pass the props as arguments

--- src/components/InputContent.jsx
+++ src/components/InputContent.jsx
@@ -1,29 +1,35 @@
 
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import InputNumber from "./input tag/InputNumber";
 import FirstUnit from "./select tag/FirstUnit";
 import LastUnit from "./select tag/LastUnit"; 
 import SwitchButton from "./switchButton/SwitchButton";
 
-export default function InputContent({ className, contents, clicked }) {
-  const [inputValue, setInputValue] = useState();
-  const [selectedFrom, setSelectedFrom] = useState("");
-  const [selectedTo, setSelectedTo] = useState("");
+import Convert from "../events/convertions/Convert";
+export default function InputContent({ className, contents, clicked, setResult, inputValue, setInputValue, selectedFrom, setSelectedFrom, selectedTo, setSelectedTo, setWeight, setHeight, weight, height, unitsFrom, setUnitsFrom, unitsTo, setUnitsTo, result, setFinalInputValue, setFinalSecondInput, setBmi}) {
+
+  const units = contents.filter(obj => obj[clicked]).map((obj) => obj[clicked][1].units).flat()
+
   useEffect(() => {
-  const validOptions = contents.find(obj => obj[clicked])?.[clicked][1].units || []; // get all units
-  // if the length is greater than 2 then set the first and the second units
-  validOptions.length >= 2 && setSelectedFrom(validOptions[0].name); setSelectedTo(validOptions[1].name); 
-  }, [clicked, contents]);
+    const validOptions = contents.find(obj => obj[clicked])?.[clicked][1].units || []; // get all units
+    setUnitsFrom(validOptions[0].value); setUnitsTo(validOptions[1].value)
+    // if the length is greater than 2 then set the first and the second units
+    validOptions.length >= 2 && setSelectedFrom(validOptions[0].name); setSelectedTo(validOptions[1].name);
+    validOptions.length >= 2 && setUnitsFrom(validOptions[0].value); setUnitsTo(validOptions[1].value);
+  }, [clicked, contents]); 
+
   return (
     <div className={className}>
-      <div className="w-full flex justify-center pb-4">
-        <InputNumber inputValue={inputValue} setInputValue={setInputValue} />
-      </div>
-      <div className="max-[520px]:justify-center gap-2  flex max-[520px]:w-[100%]">
-        <FirstUnit contents={contents} clicked={clicked} selectedFrom={selectedFrom} setSelectedFrom={setSelectedFrom} selectedTo={selectedTo}/>
-        <SwitchButton selectedFrom={selectedFrom} selectedTo={selectedTo} setSelectedFrom={setSelectedFrom} setSelectedTo={setSelectedTo}/>
-        <LastUnit contents={contents} clicked={clicked} selectedTo={selectedTo} setSelectedTo={setSelectedTo} selectedFrom={selectedFrom}/>
-        <input type="button" value="Convert" className="w-28 max-[520px]:w-full h-12 rounded-lg bg-gray-800 cursor-pointer hover:bg-slate-700 transition-all duration-150"/>
+      <div className="max-[570px]:justify-center gap-2 flex max-[640px]:flex-col-reverse w-full">
+        <div className="flex flex-col gap-2 items-center pt-4">
+          <InputNumber weight={weight} setWeight={setWeight} height={height} setHeight={setHeight} clicked={clicked} setInputValue={setInputValue} inputValue={inputValue} result={result} unitsFrom={unitsFrom}/>
+        </div>
+        <div className="w-full flex gap-2">
+          <FirstUnit className="w-full h-12 rounded-lg px-2 bg-gray-700 text-white outline-none focus:shadow-xl cursor-pointer transition-all duration-150 hover:brightness-90 relative" contents={contents} clicked={clicked} selectedFrom={selectedFrom} setSelectedFrom={setSelectedFrom} selectedTo={selectedTo}  unitsFrom={unitsFrom} setUnitsFrom={setUnitsFrom} units={units}/>
+          <SwitchButton selectedFrom={selectedFrom} selectedTo={selectedTo} setSelectedFrom={setSelectedFrom} setSelectedTo={setSelectedTo} unitsFrom={unitsFrom} setUnitsFrom={setUnitsFrom} unitsTo={unitsTo} setUnitsTo={setUnitsTo}/>
+          <LastUnit className="w-full h-12 rounded-lg px-2 bg-gray-700 text-white outline-none focus:shadow-xl cursor-pointer transition-all duration-150 hover:brightness-90 text-[clamp(0.5rem,3rem,1rem)] relative" contents={contents} clicked={clicked} selectedTo={selectedTo} setSelectedTo={setSelectedTo} selectedFrom={selectedFrom} unitsTo={unitsTo} setUnitsTo={setUnitsTo} units={units}/>
+          <input type="button" value="Convert" className="w-full max-[570px]:w-full h-12 rounded-lg bg-gray-800 cursor-pointer hover:bg-slate-700 transition-all duration-150" onClick={() => Convert(clicked, inputValue, weight, height, selectedFrom, selectedTo, setResult, setFinalInputValue, setFinalSecondInput, setInputValue, setWeight, setHeight, setBmi)}/>
+        </div>
       </div>
     </div>
   ) 
--- src/events/convertions/Convert.js
+++ src/events/convertions/Convert.js
@@ -1,4 +1,71 @@
 
-export default function Convert(){
+export default function Convert(name, inputValue, inputValue1, inputValue2, from, to, setterResult, setterFinalValue, setterFinalValue2, setterInputValue, setterInputValue1, setterInputValue2, setBmi) {
+  function Temperatures(inputValue, from, to) {
+    const value = Number(inputValue)
+    // celsius
+    if(from === 'Celsius' && to === 'Fahrenheit') {
+      setterResult((value * 1.8 + 32).toFixed(1) + '°F')
+      setterFinalValue(value + '°C');
+    };
+    if(from === 'Celsius' && to === 'Kelvin') {
+      setterResult((value + 273.15).toFixed(1) + ' K');
+      setterFinalValue(value + '°C');
+    }
+
+    // fahrenheit
+    if(from === 'Fahrenheit' && to === 'Celsius') {
+      setterResult(((value - 32) / 1.8).toFixed(1) + '°C');
+      setterFinalValue(value + '°F');
+    }
+    if(from === 'Fahrenheit' && to === 'Kelvin') {
+      setterResult(((value - 32) / 1.8 + 273.15).toFixed(1) + ' K');
+      setterFinalValue(value + '°F');
+    }
+
+    // kelvin
+    if(from === 'Kelvin' && to === 'Fahrenheit') {
+      setterResult(((value - 273.15) * 1.8 + 32).toFixed(1) + '°F');
+      setterFinalValue(value + ' K');
+    }
+    if(from === 'Kelvin' && to === 'Celsius') {
+      setterResult((value - 273.15).toFixed(1) + '°C');
+      setterFinalValue(value + ' K');
+    }
+    setterInputValue('');
+  }
+
+  function BMI(kiloNumber, heightNumber, from, to) {
+    const weight = Number(kiloNumber);
+    const height = Number(heightNumber) / 100;
+    const bmi = weight / (height * height);
+
+    console.log(from, to)
+    if (from === 'Metric' && to === 'Imperial') {
+      setterResult('BMI is ' + bmi.toFixed(2)); 
+      setterFinalValue2((weight * 2.20462).toFixed(2) + ' lbs');
+      setterFinalValue(weight + ' kg');
+      setBmi(() => {
+        if (bmi.toFixed(2) < 18.5) return 'Underweight';
+        if (bmi.toFixed(2) >= 18.5 && bmi.toFixed(2) <= 24.9) return 'Healthy Weight';
+        if (bmi.toFixed(2) >= 25 && bmi.toFixed(2) <= 29.9) return 'Overweight';
+        if (bmi.toFixed(2) >= 30) return 'Obese';
+      });
+    }
+    if (from === 'Imperial' && to === 'Metric') {
+      setterResult('Your BMI is ' + bmi.toFixed(2)); 
+      setterFinalValue2((weight / 2.20462).toFixed(2) + ' kg');
+      setterFinalValue(weight + ' lbs');
+      setBmi(() => {
+        if (bmi.toFixed(2) < 18.5) return 'Underweight';
+        if (bmi.toFixed(2) >= 18.5 && bmi.toFixed(2) <= 24.9) return 'Healthy Weight';
+        if (bmi.toFixed(2) >= 25 && bmi.toFixed(2) <= 29.9) return 'Overweight';
+        if (bmi.toFixed(2) >= 30) return 'Obese';
+      });
+    }
+    setterInputValue1('65')
+    setterInputValue2('180')
+  }
   
+  if(name === 'Temperature') return Temperatures(inputValue, from, to);
+  if(name === 'BMI') return BMI(inputValue1, inputValue2, from, to);
 }
