324ca4350465e8e7358dcdc9f8edd9d62444ffc4
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-13 20:01:26 +0300

    create a component that renders the results

---
+++ src/components/ResultContent.jsx
@@ -0,0 +1,19 @@
+
+export default function ResultContent({ classname, result, finalInputValue, clicked, finalSecondInput, bmi }) {
+  return ( 
+    <div className={ classname }>
+      {clicked === 'Temperature' && result ? 
+        <div className="flex text-2xl border py-2 px-4 rounded-lg">
+          <div>{finalInputValue}</div> 
+          <div className="px-5">=</div> 
+          <div>{result}</div>
+        </div>
+      : clicked === 'BMI' && result ? 
+        <div className="flex flex-col gap-1">
+          <div className="text-lg">{finalInputValue} = {finalSecondInput}</div>
+          <div className="text-xl">{result}</div>
+          <div className="text-xl">{bmi}</div>
+        </div>: ''}
+    </div>
+  )
+}
