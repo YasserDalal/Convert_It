d13de01725b2e72b73930644a807c25f79bea2ea
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-14 21:15:36 +0300

    add containers for results in 'Time', 'Math', 'Money'

--- src/components/ResultContent.jsx
+++ src/components/ResultContent.jsx
@@ -13,7 +13,25 @@ export default function ResultContent({ classname, result, finalInputValue, clic
           <div className="text-lg">{finalInputValue} = {finalSecondInput}</div>
           <div className="text-xl">{result}</div>
           <div className="text-xl">{bmi}</div>
-        </div>: ''}
+        </div>
+      : clicked === 'Time' && result ? 
+        <div className="flex text-2xl border py-2 px-4 rounded-lg">
+          <div>{finalInputValue}</div> 
+          <div className="px-5">=</div> 
+          <div>{result}</div>
+        </div>
+      : clicked === 'Math' && result ? 
+        <div className="flex text-2xl border py-2 px-4 rounded-lg">
+          <div>{finalInputValue}</div> 
+          <div className="px-5">=</div> 
+          <div>{result}</div>
+        </div>
+      : (clicked === 'Money' && result) && 
+        <div className="flex text-2xl border py-2 px-4 rounded-lg">
+          <div>{finalInputValue}</div> 
+          <div className="px-5">=</div> 
+          <div>{result}</div>
+        </div>}
     </div>
   )
 }
