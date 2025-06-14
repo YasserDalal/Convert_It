da9c80c499611a6c1810c9936d814f8147259951
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-14 21:17:15 +0300

    center the input tag container when the screen shrinks

--- src/components/input tag/InputNumber.jsx
+++ src/components/input tag/InputNumber.jsx
@@ -5,7 +5,7 @@ import NumberInput from "../../utils/formatter/NumberInput";
 export default function InputNumber({ inputValue, setInputValue, weight, setWeight, height, setHeight, clicked, unitsFrom }) {
   const [placeholder, setPlaceholder] = useState("0.00");
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 items-center">
       {clicked !== "BMI" ? <input type="text" inputMode="decimal" pattern="^\d{1,3}(\.\d{0,2})?$" maxLength={6} value={inputValue}  
         className="w-28 max-[520px]:w-[30%] h-12 rounded-lg bg-gray-700 px-2 text-white text-center text-2xl outline-none focus:shadow-xl focus:shadow-[#000000] transition-all duration-150 hover:brightness-90" onInput={(e) => NumberInput(e, setInputValue)} onFocus={() => setPlaceholder()} onBlur={() => setPlaceholder("0.00")} placeholder={placeholder}/>
       : (<> 
@@ -21,6 +21,5 @@ export default function InputNumber({ inputValue, setInputValue, weight, setWeig
           </div>
         </>)}
     </div>
-  
   )
 }
