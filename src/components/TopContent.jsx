df59981298ed732e66f3033edc5fcb20ec37ed06
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-17 18:28:17 +0300

    make layouts for navigation buttons for mobile devices, add expressions if data is already defined

--- src/components/TopContent.jsx
+++ src/components/TopContent.jsx
@@ -1,15 +1,29 @@
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import ClickedNav from "../events/NavButtons/ClickedNav";
 
-export default function TopContent({ className, navigatorNames, clicked, setClicked, setResult }) {
+export default function TopContent({ className, navigatorNames, clicked, setClicked, setResult, open, setOpen}) {
   return (
     <div className={className}>
       <div className="font-tab italic text-4xl flex items-center">
         <div>Convert It</div>
       </div>
-      <div className="hidden max-[720px]:block">Hi</div>
+      <div className="relative hidden max-[720px]:block left-0 right-0 z-50">
+        <FontAwesomeIcon icon={faBars} className="text-white text-4xl cursor-pointer absolute right-1 top-3" onClick={() => setOpen(!open)}/>
+      </div>
+      <div className={`bg-[#444] z-40 flex flex-col justify-center items-center gap-2 absolute top-16 right-0 transition-all duration-100 ease-in-out rounded-bl-xl rounded-br-xl ${open ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-10 pointer-events-none'}`}>
+          {navigatorNames && navigatorNames.map((item, index) => (
+          <div className={`hover:text-white transition-colors duration-150 px-10 py-4 text-2xl w-full text-center  ${clicked === `${item.name}` ? 'text-white select-none':'text-[#b0b0b0] cursor-pointer'}`} typeof="button" onClick={() => {
+            ClickedNav(setClicked, item)
+            setOpen(!open)
+            setResult()
+            }} key={index}>{item.name}
+          </div>
+        ))}
+      </div>
       <div className="flex gap-3 items-center text-lg max-[720px]:hidden">
-        {navigatorNames.map((item, index) => (
+        {navigatorNames && navigatorNames.map((item, index) => (
           <div className={`hover:text-white transition-colors duration-150 px-2 py-4 ${clicked === `${item.name}` ? 'text-white select-none':'text-[#b0b0b0] cursor-pointer'}`} typeof="button" onClick={() => {
             ClickedNav(setClicked, item)
             setResult()
