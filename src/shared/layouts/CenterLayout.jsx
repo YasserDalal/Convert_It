24d32d6d998cef62de98152e59c3f704d31b8ab7
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-17 18:22:10 +0300

    pass the props to layout components

--- src/shared/layouts/CenterLayout.jsx
+++ src/shared/layouts/CenterLayout.jsx
@@ -20,7 +20,7 @@ export default function CenterLayout({ className, contents, clicked, result, set
     <div className={className}>
       <div className="flex justify-between items-center max-[1344px]:flex-col min-w-[320px] w-full px-28 max-[1344px]:px-0 relative">
         <TitleContent classname={"pb-28 max-[1344px]:pb-38 max-[1344px]:pr-0"} contents={contents} clicked={clicked}/>
-        <ResultContent classname={"absolute -bottom-36 left-10 right-10 max-[1344px]:bottom-[30%] max-[1344px]:static max-[1344px]:left-0 max-[1344px]:right-0 max-[11344px]:pb-14  flex justify-center"} result={result} finalInputValue={finalInputValue} finalSecondInput={finalSecondInput} clicked={clicked} bmi={bmi}/>
+        <ResultContent classname={"absolute -bottom-44 left-10 right-10 max-[1344px]:bottom-[30%] max-[1344px]:static max-[1344px]:left-0 max-[1344px]:right-0 max-[11344px]:pb-14  flex justify-center"} result={result} finalInputValue={finalInputValue} finalSecondInput={finalSecondInput} clicked={clicked} bmi={bmi}/>
         <InputContent className="flex gap-2 items-end max-[570px]:flex-col max-[700px]:w-[90%] w-[639px]" contents={contents} clicked={clicked} setResult={setResult} inputValue={inputValue} setInputValue={setInputValue} selectedFrom={selectedFrom} setSelectedFrom={setSelectedFrom} selectedTo={selectedTo} setSelectedTo={setSelectedTo} setWeight={setWeight} setHeight={setHeight} weight={weight} height={height} unitsFrom={unitsFrom} setUnitsFrom={setUnitsFrom} unitsTo={unitsTo} setUnitsTo={setUnitsTo} result={result} setFinalInputValue={setFinalInputValue} setFinalSecondInput={setFinalSecondInput} setBmi={setBmi}/>
       </div>
     </div>
--- src/shared/layouts/TopLayout.jsx
+++ src/shared/layouts/TopLayout.jsx
@@ -1,10 +1,10 @@
 
 import TopContent from "../../components/TopContent"
 
-export default function TopLayout({ className, navigatorNames, clicked, setClicked, setResult }) {
+export default function TopLayout({ className, navigatorNames, clicked, setClicked, setResult, open, setOpen}) {
   return (
     <div className={className}>
-      <TopContent className="flex justify-between px-10 h-full max-[810px]:px-4" navigatorNames={navigatorNames} clicked={clicked} setClicked={setClicked} setResult={setResult}/>
+      <TopContent className="flex justify-between px-10 h-full max-[810px]:px-4" navigatorNames={navigatorNames} clicked={clicked} setClicked={setClicked} setResult={setResult} open={open} setOpen={setOpen}/>
     </div>
   )
 }
